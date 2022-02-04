ec497c6eb01d3bf35d748a23349eef5f65c5add5
Jose Lozano <lozanoje@gmail.com>
2022-02-04 18:53:26 +0100

    Added Monarch support for pooled cards!

--- babele-register.js
+++ babele-register.js
@@ -20,3 +20,20 @@ Hooks.once('init', () => {
 		
 	}
 });
+
+Hooks.on("getMonarchHandComponents", (hand, components) => {
+    components.markers.push({
+        tooltip: "En la reserva",
+        class: "pool-marker",
+        icon: "fas fa-tags",
+        color: "#00BFFF",
+        show: (card) => card.getFlag("torgeternity", "pooled")
+    });
+    components.controls.push({
+        class: "pool-toggle",
+        tooltip: "Conmutar reserva",
+        icon: "fas fa-tags",
+        color: "#00BFFF",
+        onclick: (event, card) => card.setFlag("torgeternity", "pooled", !card.getFlag("torgeternity", "pooled"))
+    })
+});
